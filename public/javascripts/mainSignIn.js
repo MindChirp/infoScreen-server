f2c234d536b84f01a3116dafa57bf3f7fb39d672
MindChirp <33499052+MindChirp@users.noreply.github.com>
2021-03-30 03:13:06 +0200

    Added register button

--- public/javascripts/mainSignIn.js
+++ public/javascripts/mainSignIn.js
@@ -40,5 +40,99 @@ function signIn(e, el) {
 function forgotPass(e) {
     e.preventDefault();
 
-    alert("Too bad.")
+    alert("Too bad.");
+}
+
+function registerNew() {
+    var form = document.getElementsByClassName("submission-form")[0]
+    var par = form.parentNode;
+    form.parentNode.removeChild(form)
+    
+    var reg = document.createElement("form");
+
+    var e = document.createElement("p");
+    e.innerHTML = "Email";
+    reg.appendChild(e);
+    var email = document.createElement("input");
+    email.type = "text";
+    email.autocomplete = "email";
+    reg.appendChild(email);
+
+    
+    var n = document.createElement("p");
+    n.innerHTML = "Full Name";
+    n.style.marginTop = "0";
+    reg.appendChild(n);
+    var name = document.createElement("input");
+    name.type = "text";
+    reg.appendChild(name);
+
+    var p = document.createElement("p");
+    p.innerHTML = "Password";
+    reg.appendChild(p);
+    var pass = document.createElement("input");
+    pass.type = "password";
+    pass.autocomplete = "none";
+    reg.appendChild(pass);
+
+    var p = document.createElement("p");
+    p.innerHTML = "Confirm Password";
+    reg.appendChild(p);
+    var pass1 = document.createElement("input");
+    pass1.type = "password";
+    pass1.autocomplete = "none";
+    reg.appendChild(pass1);
+
+
+    par.appendChild(reg);
+    reg.style.animation = "fade-in 300ms ease-in-out";
+    reg.style.animationFillMode = "both";
+
+    var subm = document.createElement("button");
+    subm.className = "sign-in smooth-shadow";
+    subm.innerHTML = "Regiser account";
+    subm.addEventListener("click", (e)=>{
+        createAccount(e, email.value, name.value, pass.value, pass1.value);
+    })
+    reg.appendChild(subm);
+
+    var back = document.createElement("button");
+    back.className="forgot-password";
+    back.innerHTML = "Back";
+    reg.appendChild(back);
+    back.addEventListener("click", (e)=>{
+        e.preventDefault();
+        location.reload();
+    })
+
+}
+
+function createAccount(event, email, name, pass, pass1) {
+    event.preventDefault();
+    //Get the credentials
+
+    if(pass!=pass1) return new Error("Passwords do not match");
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/register");
+
+    var formData = new FormData();
+    formData.append("user", name);
+    formData.append("email", email);
+    formData.append("password", pass);
+
+    xhr.send(formData);
+
+    xhr.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            //Request ok!
+            var res = JSON.parse(this.responseText);
+            location.reload();
+
+        } else if(this.readyState == 4 && this.status != 200) {
+            //Error!
+            console.log(this.responseText);
+            alert(this.responseText);
+        }
+    }
 }
